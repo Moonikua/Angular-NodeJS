887863230793880a440e8cbc40a2e44c06a7dc38
Moonikua <antonio13600@gmail.com>
2025-05-07 23:04:34 -0400

    cambios login

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { AppRoutingModule } from './app/app-routing.module';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(AppRoutingModule),
+    provideHttpClient() // ✅ correcto y moderno
+  ]
+});
